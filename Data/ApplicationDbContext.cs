64e7b31aa65d2d336b329054e84d2cf6ae696089
CarlosTorres19999 <carlos.torres@mail.com>
2022-06-27 11:16:48 -0400

    first commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Agenda.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Agenda.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        { }
+
+        public DbSet<Categoria> Categorias { get; set; }
+    }
+}
---
+++ Models/Categoria.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Agenda.Models
+{
+    public class Categoria
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="El campo Nombre es Obligatorio")]
+        public string? Nombre { get; set; }
+
+        [Display(Name= "Fecha Creacion")]
+        [DataType(DataType.Date)]
+        public DateTime FechaCreacion { get; set; }
+
+    }
+}
